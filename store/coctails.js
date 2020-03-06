78b71f521b37576e63cd80b103adf7613bec5f06
VolodymyrMazurets <volodymyrmazurets@gmail.com>
2020-03-06 18:24:05 +0200

    🐛 endpoint fix

--- store/coctails.js
+++ store/coctails.js
@@ -3,7 +3,7 @@ export const actions = {
   async getRandomCoctail({ commit }) {
     try {
       return await this.$axios.$get(
-        'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+        'https://www.thecocktaildb.com/api/json/v1/1/random.php/'
       )
     } catch (e) {
       commit('setError', e, { root: true })
